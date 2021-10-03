a9029e527ddc59ea4ed71f8b9fb4259702e1491f
Nico Krijnen <nkr@woodwing.com>
2021-10-03 20:28:58 +0200

    Police led blink

---
+++ Workshop/Police_weewoo_button/Police_weewoo_button.ino
@@ -0,0 +1,35 @@
+#define LED_A 13
+#define LED_B 3
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int blinkDelayNormal = 150;
+const int blinkDelayFast = 50;
+
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  pinMode(LED_A, OUTPUT);
+  pinMode(LED_B, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    //    digitalWrite(ledPin, HIGH);
+    blink(blinkDelayFast);
+  } else {
+    //    digitalWrite(ledPin, LOW);
+    blink(blinkDelayNormal);
+  }
+}
+
+void blink(int blinkDelay) {
+    digitalWrite(LED_A, HIGH);
+    digitalWrite(LED_B, LOW);
+    delay(blinkDelay);
+    digitalWrite(LED_A, LOW);
+    digitalWrite(LED_B, HIGH);
+    delay(blinkDelay);
+}
